3f88ae50f318d7172ce89a5bfe39b62b552296c2
jseibert <jseibert@css.edu>
2018-10-25 11:37:51 -0500

    unit 8 Participation

---
+++ SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleResponsibilityPrinciple;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Tests
+{
+    [TestClass()]
+    public class TradeProcessorTests
+    {
+        private int CountDbRecords()
+        {
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\jseibert\source\repos\cis-3285-asg-8-jackseibert\tradedatabase.mdf"";Integrated Security=True;Connect Timeout=30;"))
+            {
+                connection.Open();
+                string myScalarQuery = "SELECT COUNT(*) FROM trade";
+                SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
+                myCommand.Connection.Open();
+                int count = (int)myCommand.ExecuteScalar();
+                connection.Close();
+                return count;
+            }
+        }
+        [TestMethod()]
+        public void ProcessTradesTest()
+        {
+            Assert.Fail();
+        }
+    
+    [TestMethod()]
+    public void TestNormalFile()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.goodtrades.txt");
+            var tradeProcessor = new TradeProcessor();
+            tradeProcessor.ProcessTrades(tradeStream);
+
+            //Act
+            tradeProcessor.ProcessTrades(tradeStream);
+
+            //Assert
+            int count = CountDbRecords();
+            Assert.AreEqual(count, 4);
+        }
+    }
+}
